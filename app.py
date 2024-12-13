f2347e9898f3a1c38b2e56261ae201e1c4873dae
Angelo Basile <92684587+carni123@users.noreply.github.com>
2024-12-13 12:33:41 +0000

    COMMIT 1

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
-from flask import Flask
+from flask import Flask,render_template
+
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return render_template("home.html", titolo="home")
 
-app.run()
+@app.route("/details")
+def details():
+    dati=((1,1,1),(2,2,2),(5,3,3))
+    return render_template("details.html", titolo="details",dati=dati)
+
+
+# @app.route("/dettagli_scaffale")
+
+# def select():
+
+
+app.run(debug=True)
